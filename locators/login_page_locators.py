6c086e7aa88f2c79259de7de1eafd10a6c951cbd
AnhelinaSem <angel.angelin684@gmail.com>
2025-01-27 03:18:42 +0300

    Delete locators directory

--- locators/login_page_locators.py
+++
@@ -1,6 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class LoginPageLocators:
-    EMAIL_INPUT = (By.XPATH, "//input[@name='name']")
-    PASSWORD_INPUT = (By.XPATH, "//div[@class='input pr-6 pl-6 input_type_password input_size_default']//input[@type='password']")
-    LOGIN_BUTTON = (By.XPATH, "//button[@class ='button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa']")
